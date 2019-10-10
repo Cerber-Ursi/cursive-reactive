2641455b57e301dfc7f6d0c9c9312aa2af1a6442
Konstantin Bagrov <computers05@mail.ru>
2019-10-10 13:55:52 +0300

    [WIP] props usage draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,29 @@
 use cursive::{Cursive, view::View};
 
-type Renderer<Out> = Box<dyn Fn() -> Out>;
+type Renderer<Props, Out> = Box<dyn Fn(Props) -> Out>;
 
-pub enum Element {
-    Native(Renderer<Box<dyn View>>),
-    Reactive(Renderer<Box<dyn Component>>),
-    Fragment(Renderer<Vec<Element>>),
+pub enum Element<Props> {
+    Native(Renderer<Props, Box<dyn View>>),
+    Reactive(Renderer<Props, Box<dyn Component<Props = ()>>>),
+    Fragment(Renderer<Props, Vec<Element<()>>>),
     None,
 }
 
 pub trait Component {
-    fn render(&self) -> Element;
+    type Props;
+    fn render(&self) -> Element<Self::Props>;
 }
 
-pub fn render<C: Component + ?Sized>(comp: &C, context: &mut Cursive) {
-    mount(comp.render(), context);
+pub fn render<Props, C: Component<Props = Props> + ?Sized>(comp: &C, props: Props, context: &mut Cursive) {
+    mount(comp.render(), props, context);
 }
 
-fn mount(elem: Element, context: &mut Cursive) {
+fn mount<Props>(elem: Element<Props>, props: Props, context: &mut Cursive) {
     use Element::*;
     match elem {
-        Native(renderer) => context.add_layer(renderer()),
-        Reactive(renderer) => render(&*renderer(), context),
-        Fragment(renderer) => renderer().into_iter().for_each(|el| mount(el, context)),
+        Native(renderer) => context.add_layer(renderer(props)),
+        Reactive(renderer) => render(&*renderer(props), (), context),
+        Fragment(renderer) => renderer(props).into_iter().for_each(|el| mount(el, (), context)),
         None => {},
     };
 }
@@ -32,14 +33,15 @@ fn test() {
     use cursive::views::TextView;
     struct Test;
     impl Component for Test {
-        fn render(&self) -> Element {
-            Element::Native(Box::new(|| Box::new(TextView::new("Test"))))
+        type Props = ();
+        fn render(&self) -> Element<()> {
+            Element::Native(Box::new(|_| Box::new(TextView::new("Test"))))
         }
     }
     let backend = cursive::backend::puppet::Backend::init(Some(cursive::vec::Vec2{x: 10, y: 10}));
     let output = backend.stream();
     let mut ctx = Cursive::new(move || backend);
-    render(&Test, &mut ctx);
+    render(&Test, (), &mut ctx);
     ctx.refresh();
     ctx.step();
     output.recv().unwrap();
